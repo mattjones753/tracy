5ef76b11aee2d5624d0c45384ee78f5d0279d597
mattj <matt.jones@rightmove.co.uk>
2018-05-02 07:11:25 +0100

    attempt to make tracing more x-ray/open tracing compatible

--- src/tracy/core.clj
+++ src/tracy/core.clj
@@ -1,9 +1,14 @@
-(ns tracy.core)
+(ns tracy.core
+  (:import (java.util UUID)))
 
 (defn- default-interceptor
   "The default, empty, interceptor function.  Just calls its argument."
   [main-function]
   (main-function))
+
+(defn uuid
+  []
+  (.toString (UUID/randomUUID)))
 
 (defn compose-interceptors
   "Composes two interceptor functions, creating a new interceptor function
@@ -19,6 +24,8 @@
 
 (def ^:private thread-local-context
   (ThreadLocal.))
+(def ^:private thread-local-interceptor
+  (ThreadLocal.))
 
 (defn get-tracing-context
   []
@@ -26,17 +33,69 @@
 
 (defn set-tracing-context
   [m]
-  (.set thread-local-context m)
+  (.set thread-local-context m))
+
+(defn add-tags!
+  [m]
+  (let [current-context (get-tracing-context)
+        current-tags (:tags current-context)
+        updated-tags (merge current-tags m)]
+    (set-tracing-context
+      (merge current-context {:tags updated-tags}))))
+
+(defn get-current-interceptor
+  []
+  (or (.get thread-local-interceptor) default-interceptor))
+
+(defn set-current-interceptor
+  [interceptor]
+  (.set thread-local-interceptor interceptor))
+
+(defn new-tracing-span
+  [trace-id parent-span-id]
+  (set-tracing-context {:trace-id       (or trace-id (uuid))
+                        :span-id        (uuid)
+                        :parent-span-id parent-span-id
+                        :tags           {}})
   thread-local-context)
 
-(defmacro traced-with-context
-  [request-context interceptors & body]
+(defmacro traced
+  [trace-id parent-span-id interceptors & body]
   `(let
      [original-context# (get-tracing-context)
-      interceptor# (join-interceptors ~interceptors)]
-     (set-tracing-context ~request-context)
+      current-interceptor# (get-current-interceptor)]
+     (set-current-interceptor (join-interceptors ~interceptors))
+     (new-tracing-span ~trace-id ~parent-span-id)
      (try
-       (interceptor# (fn [] (do ~@body)))
+       ((get-current-interceptor) (fn [] (do ~@body)))
+       (finally
+         (set-tracing-context original-context#)
+         (set-current-interceptor current-interceptor#)))))
+
+(defmacro traced
+  [interceptors & body]
+  `(let
+     [original-context# (get-tracing-context)
+      current-interceptor# (get-current-interceptor)]
+     (set-current-interceptor (join-interceptors ~interceptors))
+     (new-tracing-span (uuid) nil)
+     (try
+       ((get-current-interceptor) (fn [] (do ~@body)))
+       (finally
+         (set-tracing-context original-context#)
+         (set-current-interceptor current-interceptor#)))))
+
+(def ^:dynamic blah 1)
+(binding
+  [blah "x"]
+  (println blah))
+
+(defmacro traced-inherited
+  [& body]
+  `(let
+     [original-context# (get-tracing-context)]
+     (new-tracing-span (:trace-id original-context#) (:span-id original-context#))
+     (try
+       ((get-current-interceptor) (fn [] (do ~@body)))
        (finally
          (set-tracing-context original-context#)))))
-
--- test/tracy/core_test.clj
+++ test/tracy/core_test.clj
@@ -1,52 +1,89 @@
 (ns tracy.core-test
   (:require [clojure.test :refer :all]
-            [tracy.core :as trace]))
+            [tracy.core :as tracing]))
 
 (def ^:dynamic a-variable "initial-value")
 
-(deftest tracing-with-contexts
+(defn uuid?
+  [value]
+  (re-matches #"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}" value))
 
-  (testing "can see correlation-id"
-    (trace/traced-with-context
-      {:context-key "expected value"}
-      []
-      (is (= "expected value" (:context-key (trace/get-tracing-context))))))
-
-  (testing "handles nil interceptors"
-    (trace/traced-with-context
-      {:context-key "expected value"}
-      nil
-      (is (= "expected value" (:context-key (trace/get-tracing-context))))))
-
-  (testing "outside nested tracing form uses context defined outside"
-    (trace/traced-with-context
-      {:context-key "expected value"}
-      []
-      (is (= "expected value" (:context-key (trace/get-tracing-context))))
-      (trace/traced-with-context
-        {:context-key "different expected value"}
+(let [trace-id-value (tracing/uuid)
+      parent-span-id-value (tracing/uuid)]
+  (deftest tracing-with-contexts
+    (testing "trace id and span id are used when one is set in context"
+      (tracing/traced
+        trace-id-value parent-span-id-value
         []
-        (is (=
-              "different expected value"
-              (:context-key (trace/get-tracing-context)))))
-      (is (= "expected value" (:context-key (trace/get-tracing-context))))))
+        (is (= trace-id-value (:trace-id (tracing/get-tracing-context))))
+        (is (uuid? (:span-id (tracing/get-tracing-context))))))
 
-  (testing "context is cleared on error"
-    (try
-      (trace/traced-with-context
-        {:context-key "expected value"}
+    (testing "uuid trace id is created when one is not set"
+      (tracing/traced nil nil [] (is (uuid? (:trace-id (tracing/get-tracing-context))))))
+
+
+    (testing "no parent span id is created when span is not set"
+      (tracing/traced nil nil [] (is (nil? (:parent-span-id (tracing/get-tracing-context))))))
+
+    (testing "handles nil interceptors"
+      (tracing/traced nil nil nil (is (uuid? (:trace-id (tracing/get-tracing-context))))))
+
+    (testing "span tags are not propagated to sub-spans"
+      (tracing/traced
+        trace-id-value parent-span-id-value
         []
-        (is (= "expected value" (:context-key (trace/get-tracing-context))))
-        (throw (Exception. "my exception message"))
-        )
-      (catch Exception ignored))
-    (is (nil? (:context-key (trace/get-tracing-context)))))
+        (tracing/add-tags! {:key "value"})
+        (is (= {:key "value"} (:tags (tracing/get-tracing-context))))
+        (tracing/traced-inherited
+          (is (= {} (:tags (tracing/get-tracing-context)))))))
 
-  (testing "tracing interceptors should be executed as
-  part of trace when provided"
-    (trace/traced-with-context
-      {:context-key "expected value"}
-      [(fn [f]
-         (binding [a-variable "overriden-value"] (f)))]
-      (is (= "expected value" (:context-key (trace/get-tracing-context))))
-      (is (= "overriden-value" a-variable)))))
+    (testing "new span ids are generated for sub-spans and previous set to parent-span-id"
+      (tracing/traced
+        trace-id-value parent-span-id-value []
+        (is (= parent-span-id-value (:parent-span-id (tracing/get-tracing-context))))
+        (let [outer-span-id (:span-id (tracing/get-tracing-context))]
+          (tracing/traced-inherited
+            (is (not (= parent-span-id-value (:span-id (tracing/get-tracing-context)))))
+            (is (uuid? (:span-id (tracing/get-tracing-context))))
+            (is (= outer-span-id (:parent-span-id (tracing/get-tracing-context))))))))
+
+    (testing "inherited with nothing to inherit uses new ids and empty parent"
+      (is (= {} (tracing/get-tracing-context)))
+      (tracing/traced-inherited
+        (is (uuid? (:trace-id (tracing/get-tracing-context))))
+        (is (uuid? (:span-id (tracing/get-tracing-context))))
+        (is (nil? (:parent-span-id (tracing/get-tracing-context))))))
+
+    (testing "trace ids are propagated to sub-spans"
+      (tracing/traced
+        trace-id-value nil []
+        (is (= trace-id-value (:trace-id (tracing/get-tracing-context))))
+        (tracing/traced-inherited
+          (is (= trace-id-value (:trace-id (tracing/get-tracing-context)))))))
+
+    (testing "context is cleared on error"
+      (try
+        (tracing/traced
+          trace-id-value nil
+          []
+          (is (= trace-id-value (:trace-id (tracing/get-tracing-context))))
+          (throw (Exception.)))
+        (catch Exception _))
+      (is (nil? (:trace-id (tracing/get-tracing-context)))))
+
+    (testing "tracing interceptors should be executed as part of trace when provided"
+      (tracing/traced
+        trace-id-value nil
+        [(fn [f]
+           (binding [a-variable (:trace-id (tracing/get-tracing-context))] (f)))]
+        (is (= trace-id-value a-variable))))
+
+    (testing "tracing interceptors should be executed as part of inherited trace when provided"
+      (binding [a-variable 1]
+        (tracing/traced
+          trace-id-value nil
+          [(fn [f]
+             (binding [a-variable (+ a-variable 1)] (f)))]
+          (is (= 2 a-variable))
+          (tracing/traced-inherited
+            (is (= 3 a-variable))))))))
